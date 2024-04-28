8cce95879f580c2a77e25adde5d52da7277a0ed7
Ednut001 <147618496+Ednut001@users.noreply.github.com>
2024-04-28 23:17:46 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,13 +10,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "‪2349048288932"
+global.ownernomer = "2349048288932"
 global.ownername = "EDNUT HIMSELF"
 global.ytname = "YT: Ednut001"
 global.socialm = "GitHub: Ednut001"
 global.location = "nigeria, lagos, abuja"
 
-global.ownernumber = '2349048288932‪‬'  //creator number
+global.ownernumber = '2349048288932'  //creator number
 global.ownername = 'EDNUT' //owner name
 global.botname = 'THA IKA X' //name of the bot
 
